0386a89ff0fb2a1d406a94b0fa0ca0500e775e3e
momochyann <102209169+momochyann@users.noreply.github.com>
2025-01-07 11:19:17 +0800

    退出按钮修改
    场景按钮增加点击退出功能  按钮退出函数合并函数

--- Assets/知识问答资源/脚本/ReturnButton.cs
+++ Assets/知识问答资源/脚本/ReturnButton.cs
@@ -8,21 +8,19 @@ public class ReturnButton : MonoBehaviour, _ISeriportInput
     List<int> clickedbuttonIndex = new List<int>();
     float timeout = 0;
     // Start is called before the first frame update
-    private void Update() { //更新函数
-        if(Time.time - timeout > 2f) //如果时间大于2秒
+    private void Update()
+    { //更新函数
+        if (Time.time - timeout > 2f) //如果时间大于2秒
         {
             clickedbuttonIndex.Clear(); //清空列表
         }
-        if(clickedbuttonIndex.Count == 4)  //如果同时按下4个按钮
-        {
-            SceneManager.LoadScene(0);//返回主菜单
-        }
+        checkButton();
     }
     // Update is called once per frame
-   
+
     public void OnDataReceived(int buttonIndex) //串口输入时处理函数
     {
-        if(clickedbuttonIndex.Contains(buttonIndex)) //如果列表中包含这个索引   
+        if (clickedbuttonIndex.Contains(buttonIndex)) //如果列表中包含这个索引   
         {
             return;
 
@@ -30,5 +28,11 @@ public class ReturnButton : MonoBehaviour, _ISeriportInput
         clickedbuttonIndex.Add(buttonIndex); //添加到列表
         timeout = Time.time; //设置时间
     }
-
+    public void checkButton()
+    {
+        if (clickedbuttonIndex.Count == 4)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
 }
